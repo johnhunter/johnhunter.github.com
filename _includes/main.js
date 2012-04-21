df1f912336623ffe4860938046755c330e3f75e6
John Hunter <john@johnhunter.info>
2012-04-21 18:56:52 +0100

    Add initial structure.

---
+++ _includes/main.js
@@ -0,0 +1,12 @@
+/**
+	main.js
+
+	@author       John Hunter for johnhunter.info
+	Created       2012-04-21
+	Licence       CC-BSD (2012) <http://creativecommons.org/licenses/BSD/>
+	
+	@dependency   
+	
+**/
+
+console.log(window);
